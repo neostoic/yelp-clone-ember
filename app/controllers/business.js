df8b0473045c86438e7ee59ac96ae6ca2655bbe4
Kim Dang & jocelyn alsdorf <student@epicodus.com>
2015-06-17 16:42:35 -0700

    end of day

--- app/controllers/business.js
+++ app/controllers/business.js
@@ -15,6 +15,18 @@ export default Ember.Controller.extend({
       business.set('description', this.get('model.description'));
       business.save();
       this.set('isEditing', false);
+    },
+    delete: function() {
+      var business = this.get('model');
+      var reviews = this.get('reviews'),
+        list = reviews.toArray();
+      list.forEach(function(review){
+        review.destroyRecord();
+        reviews.removeObject(review);
+      });
+      // var reviews = this.get('controllers.review.model');
+      business.destroyRecord();
+      this.transitionToRoute('businesses');
     }
   }
 });
