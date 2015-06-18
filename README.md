ea6d10c45cd9c12f29581167fb360b787d7b8baa
Kim Dang & jocelyn alsdorf <student@epicodus.com>
2015-06-18 10:40:26 -0700

    add functionality to delete posts.

--- README.md
+++ README.md
@@ -3,6 +3,19 @@
 This README outlines the details of collaborating on this Ember application.
 A short introduction of this app could easily go here.
 
+## Setup
+
+clone the project
+then enter this command in the terminal:
+```
+$ npm intstall && install
+```
+start up the server with:
+```
+$ ember serve
+```
+then in a browser go to localhost:4200
+
 ## Prerequisites
 
 You will need the following things properly installed on your computer.
@@ -50,4 +63,3 @@ Specify what it takes to deploy your app.
 * Development Browser Extensions
   * [ember inspector for chrome](https://chrome.google.com/webstore/detail/ember-inspector/bmdblncegkenkacieihfhpjfppoconhi)
   * [ember inspector for firefox](https://addons.mozilla.org/en-US/firefox/addon/ember-inspector/)
-
--- app/controllers/business.js
+++ app/controllers/business.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   isEditing: false,
-
+  needs: ['review'],
   actions: {
     edit: function() {
       this.set('isEditing', true);
@@ -18,14 +18,24 @@ export default Ember.Controller.extend({
     },
     delete: function() {
       var business = this.get('model');
-      // var reviews = this.get('reviews'),
-      //   list = reviews.toArray();
-      // list.forEach(function(review){
-      //   review.destroyRecord();
-      //   reviews.removeObject(review);
-      // });
-      // var reviews = this.get('controllers.review.model');
-      business.destroyRecord();
+      var reviews = business.get('reviews');
+
+      var x = [];
+      reviews.forEach(function(review){
+        x.push(review.get('id'));
+      });
+
+      var allReviews = this.store.find('review').then(function(allReviewsCallback) {
+        x.forEach(function(id){
+          allReviewsCallback.forEach(function(aReview){
+            if(id === aReview.get('id')) {
+              aReview.destroyRecord();
+            }
+          });
+        });
+        business.destroyRecord();
+      });
+
       this.transitionToRoute('businesses');
     }
   }
